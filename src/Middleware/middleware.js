b77c1830d186510b188222e5af203c7416c4dc72
vishanksinghh <109924706+vishanksinghh@users.noreply.github.com>
2023-04-03 18:11:40 +0530

    middleware Update

--- src/Middleware/middleware.js
+++ src/Middleware/middleware.js
@@ -1,13 +1,8 @@
 const jwt = require("jsonwebtoken");
 const blogModel = require("../modules/bloggModel")
-const validator = require("validator")
+const {isValidObjectId}=require('mongoose');
 
-const idcheck = function(value) {
-    let a = validator.isMongoId(value)
-    if (!a) {
-        return true
-    } else return false
-}
+
 
 const auth = function(req, res, next) {
 
@@ -15,11 +10,11 @@ const auth = function(req, res, next) {
        
         let token = req.headers["x-api-key"]
 
-        if (!token) { return res.status(401).send({ status: false, msg: "Token must be present in request headers" }) }
+        if (!token) { return res.status(401).send({ status: false, message: "Token must be present in request headers" }) }
   
         jwt.verify(token, "californium-blog",function(err, decodedToken){ 
             
-            if(err) return res.status(401).send({ status: false, msg: "Token is Incorrect" })
+            if(err) return res.status(401).send({ status: false, message: "Token is Incorrect" })
             req.token = decodedToken.authorId
     
             next()
@@ -28,7 +23,7 @@ const auth = function(req, res, next) {
         
 
     } catch (error) {
-        return res.status(500).send({ status: false, msg: error.message })
+        return res.status(500).send({ status: false, message: error.message })
     }
 }
 
@@ -40,21 +35,21 @@ const authorisation = async function(req, res, next) {
       
         //let token = req.headers["x-api-key"]
         const blogId = req.params.blogId
-        if(idcheck(req.params.blogId)) return res.status(404).send({status:false,msg:"ID Incorrect"});
+        if(!isValidObjectId(req.params.blogId)) return res.status(400).send({status:false,message:"ID Incorrect"});
         const blog = await blogModel.findById(blogId).select({ authorId: 1, _id: 0 })
-         if (blog == null) {
+         if (!blog) {
+             return res.status(404).send({ status: false, message: "Blog document doesn't exist.." })
             
-             return res.status(404).send({ status: false, msg: "Blog document doesn't exist.." })
          }
 
         const authorId = blog.authorId.toString()
         //const decodedToken = jwt.verify(token, "californium-blog")
         if (authorId != req.token) {
-            return res.status(403).send({ status: false, msg: 'Access is Denied' })
+            return res.status(403).send({ status: false, message: 'Access is Denied' })
         }
         
     } catch (error) {
-        return res.status(500).send({ status: false, msg: error.message })
+        return res.status(500).send({ status: false, message: error.message })
     }
     next()
 }
